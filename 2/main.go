f0df1eee498f4ee0efd50a5c377f558c66292f4c
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2024-02-29 14:59:22 -0300

    first commit

---
+++ 2/main.go
@@ -0,0 +1,18 @@
+package main
+
+const a = "Hello, World"
+
+var (
+	b bool   = true
+	c int    = 10
+	d string = "Vinícius"
+)
+
+func main() {
+	nome := "vinicius"
+	println(nome)
+	println("Mensagem: " + a)
+	println(b)
+	println(c)
+	println(d)
+}
---
+++ 3/main.go
@@ -0,0 +1,20 @@
+package main
+
+type ID int
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Vinícius"
+	e float64 = 1.2
+	f ID      = 1
+)
+
+func main() {
+	nome := "vinicius" //string
+	println(nome)
+	println(b)
+	println(c)
+	println(d)
+	println(f)
+}
